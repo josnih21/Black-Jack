8d6775718a53e3edc5744dce6fe2b477986615c7
José Ángel <josnih@gmail.com>
2020-05-13 14:16:46 +0100

    refactor properties to types.ts

--- src/components/Greetings/Greetings.tsx
+++ src/components/Greetings/Greetings.tsx
@@ -1,9 +1,5 @@
-import * as React from 'react';
-
-interface GreetingsProps {
-    name:string
-    lastName?:string
-}
+import * as React from "react";
+import { GreetingsProps } from "./types";
 
 export const Greetings:React.FC<GreetingsProps> = ({name,lastName}) => (
 <h1>Hello, {name}{lastName}</h1>
