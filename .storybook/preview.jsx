b00e026f8b8409c7ccd85c6ede2a7ea3e4fb61e4
José Ángel <josnih@gmail.com>
2020-05-14 13:55:31 +0100

    config storybook addons

--- .storybook/preview.jsx
+++ .storybook/preview.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { addDecorator } from '@storybook/react';
+import { reduceEachLeadingCommentRange } from 'typescript';
 
 const styles = {
   fontFamily: 'Segoe UI, Roboto, Oxygen, Ubuntu , Cantarell, Fira Sans, Droid Sans, Helvetica Neue',
--- src/components/Greetings/Greetings.stories.tsx
+++ src/components/Greetings/Greetings.stories.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 
 import {Greetings} from "./Greetings";
+import {text,withKnobs} from "@storybook/addon-knobs";
+import {withA11y} from "@storybook/addon-a11y";
 
 export default {
   title: 'HelloUser',
+  decorators: [withKnobs,withA11y]
 };
 
 export const withName = () => <Greetings name={'Josnih'}/>;
